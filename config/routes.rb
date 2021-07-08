c5ba8e24c883240d6f95b8a583e166f00a6da01a
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-08 22:20:55 +0900

    feat:usersとcontrollersのroutes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'top#index'
+  get 'medicalstaffs/index'
+  get 'users/index'
   devise_for :medicalstaffs
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
