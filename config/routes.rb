7568b0dc42ff94f6c62568148856a0d842f41596
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-10 17:26:55 +0900

    feat:ユーザー編集画面へのリンク、ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'top#index'
   get 'medicalstaffs/index'
   get 'users/index'
-  devise_for :medicalstaffs
   devise_for :users
+  resources :users, only: [:index, :edit]
+  devise_for :medicalstaffs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
