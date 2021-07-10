c3ebe9b6284a5cbc79d387968efdf6f169a7e1da
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-11 04:18:07 +0900

    feat:medicalstaffのindex,edit,updateのviewとcontrollerを作成,ルーティングの設定

--- app/controllers/medicalstaffs_controller.rb
+++ app/controllers/medicalstaffs_controller.rb
@@ -1,4 +1,19 @@
 class MedicalstaffsController < ApplicationController
   def index
+    @staff = Medicalstaff.find(current_medicalstaff.id)
+  end
+
+  def edit
+    @staff = Medicalstaff.find(current_medicalstaff.id)
+  end
+
+  def update
+    @staff = medicalstaff.find(current_medicalstaff.id)
+    if @staff.update(params.require(:medicalstaff).permit(:name, :address))
+      flash[:notice] = "プロフィール情報を更新しました"
+      redirect_to edit_medicalstaff_path(current_medicalstaff)
+    else
+      render "edit"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'top#index'
-  get 'medicalstaffs/index'
   devise_for :users
   resources :users, only: [:index, :edit, :update]
   devise_for :medicalstaffs
+  resources :medicalstaffs, only: [:index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
