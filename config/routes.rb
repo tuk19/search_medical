d4956d9cd263040e9568041fa0019c9a97ad5578
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-11 00:50:14 +0900

    feat:userのindexとeditのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root to: 'top#index'
   get 'medicalstaffs/index'
-  get 'users/index'
   devise_for :users
-  resources :users, only: [:index, :edit]
+  resources :users, only: [:index, :edit, :update]
   devise_for :medicalstaffs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
