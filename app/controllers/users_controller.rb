4d91681caecc078dfdbf15a1de866d66f69a5d69
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-10 17:16:15 +0900

    feat:users_controllerにindexとeditを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+  end
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(params.require(:user).permit(:name, :email, :password, :image))
+      flash[:notice] = "プロフィール情報を更新しました"
+      redirect_to edit_user_path(current_user)
+    else
+      render "edit"
+    end
   end
 end
