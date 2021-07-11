455b7fa19c205cb6b1008cd60177b6933a358d1b
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-12 04:07:22 +0900

    fix:institutionのルーティングをresourcesに

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'institutions/index'
   root to: 'top#index'
   devise_for :users
   resources :users, only: [:index, :edit, :update]
   devise_for :medicalstaffs
   resources :medicalstaffs, only: [:index, :edit, :update]
+  resources :institutions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
