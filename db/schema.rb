dfc19632b3156195d24f5312d3012d73aeced891
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-12 03:58:05 +0900

    feat:institutionモデルにimageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_183224) do
+ActiveRecord::Schema.define(version: 2021_07_11_185715) do
 
   create_table "institutions", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_11_183224) do
     t.string "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "medicalstaffs", force: :cascade do |t|
