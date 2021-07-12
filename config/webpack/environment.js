81eb6022a5cae45c3342a39167402f179f8f3b28
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-13 04:46:33 +0900

    feat:jquery導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = required('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
 module.exports = environment
