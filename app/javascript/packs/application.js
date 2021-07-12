e6ef49c3bff0916234ec2f07d5914c1d87badf4f
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-13 08:41:42 +0900

    feat:jpostalによる住所検索の自動化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,21 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
+import "packs/jquery.jpostal.js"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+jQuery(document).on("turbolinks:load", function() {
+  $('#institution_postal_code').jpostal({
+    postcode : [
+      '#institution_postal_code'
+    ],
+    address: {
+      "#institution_prefecture_code": "%3", // # 都道府県が入力される
+      "#institution_city"           : "%4%5", // # 市区町村と町域が入力される
+      "#institution_street"         : "%6%7" // # 大口事務所の番地と名称が入力される
+    }
+  });
+});
