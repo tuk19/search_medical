d73688e7e67988573cd12abb4c93efd8712994b6
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-12 17:11:36 +0900

    feat:institutionの各controllerを作成

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -1,16 +1,47 @@
 class InstitutionsController < ApplicationController
   def index
+    max_num = 50
+    if Institution.count > 50
+      @institution = Institution.limit(max_num)
+    else
+      @institution = Institution.all
+    end
   end
 
   def new
+    @institution = Institution.new
   end
 
   def create
+    @institution = Institution.new(institution_params)
+    if @institution.save
+      redirect_to institutions_path, notice: "医療機関を追加しました"
+    else
+      render "new"
+    end
   end
 
   def edit
+    @institution = Institution.find(params[:id])
   end
 
   def update
+    @institution = Institution.find(params[:id])
+    if @institution.update(institution_params)
+      redirect_to edit_institution_path(params[:id]), notice: "医療機関情報を更新しました"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @institution = Institution.find(params[:id])
+    @institution.destroy
+    redirect_to institutions_path, notice: "医療機関を削除しました"
+  end
+
+  private
+  def institution_params
+    params.require(:institution).permit(:name, :address, :introduction, :image)
   end
 end
