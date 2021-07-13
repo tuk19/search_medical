5212210f8d265bbd19a4c662a6032b9954634c3e
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-13 21:50:15 +0900

    fea:institutionにimageuploaderを追加

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,4 +1,6 @@
 class Institution < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   def join_address
     "#{self.prefecture}#{self.address_city}#{self.address_street}#{self.address_building}"
   end
