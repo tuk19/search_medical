30472dec1cac492007e2ac651247123eea62fffb
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-18 23:04:38 +0900

    fix:request/top_specを修正

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Tops", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/top/index"
+
+    before do
+      get root_path
+    end
+
+    example "topページへアクセスできること"  do
       expect(response).to have_http_status(:success)
+    end
+
+    example "ログイン機能が含まれること" do
+      expect(response.body).to include("ログイン")
     end
   end
 
