4ada2e6ae206649697781f12d2fa06db2c37f974
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-19 14:53:01 +0900

    fix:requests/users_specにeditのテストを追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,20 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :request do
-  let!(:user) { create(:testuser) }
+RSpec.describe "Users", type: :request do
+  let(:user) { create(:testuser) }
+
   describe "GET /index" do
     before do
       sign_in user
       get users_path
     end
 
-    it "indexページが表示できること" do
+    example "indexページが表示できること" do
       expect(response).to have_http_status(:success)
     end
 
-    it "ユーザー情報編集が含まれること" do
-      expect(response.body).to include("編集")
+    example "ユーザー情報が含まれること" do
+      binding.pry
+      expect(response.body).to include(user.name)
+      expect(response.body).to include(user.email)
     end
   end
 
+  describe "GET/edit" do
+    before do
+      sign_in user
+      get edit_user_path(user.id)
+    end
+
+    example "editページが表示できること" do
+      expect(response).to have_http_status(:success)
+    end
+
+    example "ユーザー情報編集画面が表示されること" do
+      expect(response.body).to include("ユーザープロフィール編集")
+    end
+  end
 end
