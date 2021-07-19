00500d1dfb0f5620b925d9e6416d561f47d45811
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-19 14:52:18 +0900

    feat:medicalstaff_request_specを追加

--- spec/requests/medicalstaffs_spec.rb
+++ spec/requests/medicalstaffs_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Medicalstaffs", type: :request do
+  let(:medicalstaff) { create(:teststaff) }
   describe "GET /index" do
-    it "returns http success" do
-      get "/medicalstaffs/index"
+    before do
+      sign_in medicalstaff
+      get medicalstaffs_path
+    end
+
+    example "indexページが表示できること" do
       expect(response).to have_http_status(:success)
+    end
+
+    example "スタッフ情報編集画面が含まれること" do
+      expect(response.body).to include("編集")
     end
   end
 
