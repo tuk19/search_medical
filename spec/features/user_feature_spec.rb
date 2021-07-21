4934c31fb149d0695606abc62cbb09b984815148
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-21 10:18:10 +0900

    fix:user_feature_specにdescribe user_login scenario 'プロフィール画面からアカウント情報編集画面へ遷移し、プロフィール画面へ戻れるか' scenario 'アカウント編集画面からアカウント情報が更新できるか'を追加

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -3,11 +3,61 @@ require 'rails_helper'
 RSpec.feature 'Users_Features', type: :feature do
   include Devise::Test::IntegrationHelpers
 
+  describe "user_login" do
+    let(:user) { create(:testuser)}
+
+    scenario "新規作成できるか" do
+      visit new_user_registration_path
+      expect(page).to have_content("患者様・ご家族様新規登録")
+      fill_in "user_registration_email", with: "example@example.com"
+      fill_in "user_registration_password", with: "foobar"
+      fill_in "user_registration_confirm_password", with: "foobar"
+      click_button "新規登録"
+      expect(page).to have_content("ログインしています")
+    end
+
+    scenario 'ログインできるか' do
+      visit user_session_path
+      expect(page).to have_content("患者様・ご家族様ログイン")
+      fill_in "user_login_email", with: user.email
+      fill_in "user_login_password", with: user.password
+      click_button "ログイン"
+      expect(page).to have_content("ログインしています")
+    end
+  end
+
+
   describe "link_to_users" do
     let(:user) { create(:testuser) }
 
     background do
       sign_in user
+    end
+
+    scenario 'プロフィール画面からアカウント情報編集画面へ遷移し、プロフィール画面へ戻れるか' do
+      visit users_path
+      expect(page).to have_content(user.name)
+      expect(page).to have_content(user.email)
+      expect(page).to have_content("アカウント情報を編集する")
+      expect(page).to have_content("プロフィールを編集する")
+      click_link "アカウント情報を編集する"
+      expect(page).to have_content("アカウント情報変更")
+      click_link "戻る"
+      expect(page).to have_content(user.name)
+      expect(page).to have_content(user.email)
+      expect(page).to have_content("アカウント情報を編集する")
+      expect(page).to have_content("プロフィールを編集する")
+    end
+
+    scenario 'アカウント編集画面からアカウント情報が更新できるか' do
+      visit edit_user_registration_path
+      expect(page).to have_content("アカウント情報変更")
+      fill_in 'edit_user_registratin_email', with: "edituser@user.com"
+      fill_in 'edit_user_registratin_password', with: "foobarbaz"
+      fill_in 'edit_user_registratin_confirm_password', with: "foobarbaz"
+      fill_in 'edit_user_current_password', with: user.password
+      click_button "アカウント情報を更新する"
+      expect(page).to have_content("Your account has been updated successfully")
     end
 
     scenario 'プロフィール画面からプロフィール編集画面へ遷移できるか' do
