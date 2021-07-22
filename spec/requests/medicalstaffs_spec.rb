a5887b690da01395c714169d7289f59059bef9ee
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-22 23:40:25 +0900

    feat:updateテストを追加、before_action :authenticateのテストを追加

--- spec/requests/medicalstaffs_spec.rb
+++ spec/requests/medicalstaffs_spec.rb
@@ -4,17 +4,26 @@ RSpec.describe "Medicalstaffs", type: :request do
   let(:medicalstaff) { create(:teststaff) }
 
   describe "GET /index" do
-    before do
-      sign_in medicalstaff
-      get medicalstaffs_path
+    context "ログインしていない場合" do
+      example "ログインページへリダイレクトすること" do
+        get medicalstaffs_path
+        expect(response).to redirect_to medicalstaff_session_path
+      end
     end
 
-    example "indexページが表示できること" do
-      expect(response).to have_http_status(:success)
-    end
+    context "ログインしている場合" do
+      before do
+        sign_in medicalstaff
+        get medicalstaffs_path
+      end
 
-    example "スタッフ情報が含まれること" do
-      expect(response.body).to include(medicalstaff.email)
+      example "indexページが表示できること" do
+        expect(response).to have_http_status(:success)
+      end
+
+      example "スタッフ情報が含まれること" do
+        expect(response.body).to include(medicalstaff.email)
+      end
     end
   end
 
@@ -30,6 +39,23 @@ RSpec.describe "Medicalstaffs", type: :request do
 
     example "スタッフ情報編集画面が表示されること" do
       expect(response.body).to include("登録情報編集")
+    end
+  end
+
+  describe 'PUT /create' do
+    before do
+      sign_in medicalstaff
+    end
+
+    example 'リクエストが成功すること' do
+      put medicalstaff_path(medicalstaff.id), params: { medicalstaff: { name: "examplestaff", address: "exampleaddress" } }
+      expect(response.status).to eq 302
+    end
+
+    example 'ユーザー名が更新されること' do
+      expect do
+        put medicalstaff_path(medicalstaff.id), params: { medicalstaff: { name: "examplestaff", address: "exampleaddress" } }
+      end.to change { Medicalstaff.find(medicalstaff.id).name }.from('teststaff').to('examplestaff')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,18 +4,27 @@ RSpec.describe "Users", type: :request do
   let(:user) { create(:testuser) }
 
   describe "GET /index" do
-    before do
-      sign_in user
-      get users_path
+    context "ログインしていない場合" do
+      example "ログインページへリダイレクトすること" do
+        get users_path
+        expect(response).to redirect_to user_session_path
+      end
     end
 
-    example "indexページが表示できること" do
-      expect(response).to have_http_status(:success)
-    end
+    context "ログインしている場合" do
+      before do
+        sign_in user
+        get users_path
+      end
 
-    example "ユーザー情報が含まれること" do
-      expect(response.body).to include(user.name)
-      expect(response.body).to include(user.email)
+      example "indexページが表示できること" do
+        expect(response).to have_http_status(:success)
+      end
+
+      example "ユーザー情報が含まれること" do
+        expect(response.body).to include(user.name)
+        expect(response.body).to include(user.email)
+      end
     end
   end
 
@@ -31,6 +40,23 @@ RSpec.describe "Users", type: :request do
 
     example "ユーザー情報編集画面が表示されること" do
       expect(response.body).to include("ユーザープロフィール編集")
+    end
+  end
+
+  describe 'PUT /update' do
+    before do
+      sign_in user
+    end
+
+    example 'リクエストが成功すること' do
+      put user_path(user.id), params: { user: { name: "exampleuser", image: "image.png" } }
+      expect(response.status).to eq 302
+    end
+
+    example 'ユーザー名が更新されること' do
+      expect do
+        put user_path(user.id), params: { user: { name: "exampleuser", image: "image.png" } }
+      end.to change { User.find(user.id).name }.from('testuser').to('exampleuser')
     end
   end
 end
