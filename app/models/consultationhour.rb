1d661f0d88e0db3d66702a5bdc367ba514a034e3
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-30 01:37:35 +0900

    fix: バリデーションの formatを変更

--- app/models/consultationhour.rb
+++ app/models/consultationhour.rb
@@ -2,5 +2,5 @@ class Consultationhour < ApplicationRecord
   belongs_to :institution
 
   validates :start_time, presence: true, format: { with: /[[0-2]0-9]:[0-5][0-9]/, message: "開始時間は[00:00]の形式で入力してください" }
-  validates :end_time, presence: true, format: { with: /\d{2}:\d{2}/ }
+  validates :end_time, presence: true, format: { with: /[[0-2]0-9]:[0-5][0-9]/, message: "開始時間は[00:00]の形式で入力してください" }
 end
