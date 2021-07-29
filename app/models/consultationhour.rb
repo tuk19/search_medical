4bc9a20850d0068ce5363359221d81e00d742c3e
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-30 03:11:41 +0900

    feat: 開始時間と終了時間の差をバリデーションに追加。エラーメッセージを日本語で表示

--- app/models/consultationhour.rb
+++ app/models/consultationhour.rb
@@ -3,4 +3,13 @@ class Consultationhour < ApplicationRecord
 
   validates :start_time, presence: true, format: { with: /[[0-2]0-9]:[0-5][0-9]/, message: "開始時間は[00:00]の形式で入力してください" }
   validates :end_time, presence: true, format: { with: /[[0-2]0-9]:[0-5][0-9]/, message: "開始時間は[00:00]の形式で入力してください" }
+  validate :before_start_time
+
+  def before_start_time
+    if start_time != nil && end_time != nil
+      if end_time < start_time
+        errors.add(:end_time, "は開始時間よりも後に設定してください")
+      end
+    end
+  end
 end
