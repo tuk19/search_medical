975c6086a229d22dcc3d2c5d461b9600c740a0b5
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-30 00:19:01 +0900

    feat: consultationhoursのspecとhelperを作成

---
+++ app/helpers/consultationhours_helper.rb
@@ -0,0 +1,2 @@
+module ConsultationhoursHelper
+end
---
+++ spec/factories/consultationhours.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :consultationhour do
+    start_time { "2021-07-29 19:48:33" }
+    end_time { "2021-07-29 19:48:33" }
+    monday { "MyString" }
+    tuesday { "MyString" }
+    wednesday { "MyString" }
+    thursday { "MyString" }
+    friday { "MyString" }
+    saturday { "MyString" }
+    sunday { "MyString" }
+    holiday { "MyString" }
+  end
+end
