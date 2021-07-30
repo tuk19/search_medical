d48ec0ba653761e9ae7355095f60693d32c5d9e4
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-30 22:06:02 +0900

    feat: start_timeを日本語表示に変更

--- app/models/consultationhour.rb
+++ app/models/consultationhour.rb
@@ -1,8 +1,7 @@
 class Consultationhour < ApplicationRecord
   belongs_to :institution
-
-  validates :start_time, presence: true, format: { with: /[[0-2]0-9]:[0-5][0-9]/, message: "開始時間は[00:00]の形式で入力してください" }
-  validates :end_time, presence: true, format: { with: /[[0-2]0-9]:[0-5][0-9]/, message: "開始時間は[00:00]の形式で入力してください" }
+  validates :start_time, presence: true, format: { with: /[[0-2]0-9]:[0-5][0-9]/, message: "は[00:00]の形式で入力してください" }
+  validates :end_time, presence: true, format: { with: /[[0-2]0-9]:[0-5][0-9]/, message: "は[00:00]の形式で入力してください" }
   validate :before_start_time
 
   def before_start_time
