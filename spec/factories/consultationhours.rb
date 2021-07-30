537344aba26f625e8ce46433acc03e08de8096cf
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-30 21:06:44 +0900

    fix: factories_consultationhoursの修正

--- spec/factories/consultationhours.rb
+++ spec/factories/consultationhours.rb
@@ -1,14 +1,15 @@
 FactoryBot.define do
   factory :consultationhour do
-    start_time { "2021-07-29 19:48:33" }
-    end_time { "2021-07-29 19:48:33" }
-    monday { "MyString" }
-    tuesday { "MyString" }
-    wednesday { "MyString" }
-    thursday { "MyString" }
-    friday { "MyString" }
-    saturday { "MyString" }
-    sunday { "MyString" }
-    holiday { "MyString" }
+    start_time { "2021-07-29 8:30:00" }
+    end_time { "2021-07-29 12:30:00" }
+    monday { "●" }
+    tuesday { "●" }
+    wednesday { "●" }
+    thursday { "-" }
+    friday { "●" }
+    saturday { "※" }
+    sunday { "-" }
+    holiday { "-" }
+    detail { "土曜日は13:00まで" }
   end
 end
