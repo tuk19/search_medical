ed4cb3895036dcc6a3e5925c7a8d12899673b36a
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-07-30 23:05:53 +0900

    feat: パラメータが異なるfactories/consultationhoursを追加

--- spec/factories/consultationhours.rb
+++ spec/factories/consultationhours.rb
@@ -12,4 +12,19 @@ FactoryBot.define do
     holiday { "-" }
     detail { "土曜日は13:00まで" }
   end
+
+  factory :editconsultationhour, class: "Consultationhour" do
+    start_time { "2021-07-29 8:30:00" }
+    end_time { "2021-07-29 13:00:00" }
+    monday { "●" }
+    tuesday { "●" }
+    wednesday { "※" }
+    thursday { "-" }
+    friday { "●" }
+    saturday { "●" }
+    sunday { "※" }
+    holiday { "-" }
+    detail { "※は12:30まで" }
+    institution_id { "1" }
+  end
 end
