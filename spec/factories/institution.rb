a388e644e14bf38f091d21f453c63e94fccda74a
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-08-01 15:55:17 +0900

    feat: osakainstitutionを追加

--- spec/factories/institution.rb
+++ spec/factories/institution.rb
@@ -8,4 +8,15 @@ FactoryBot.define do
     address_building { "東京ビル1F" }
     address { "東京都東京区東京1-2-3東京ビル1F" }
   end
+
+  factory :osakainstitution, class: "Institution" do
+    name { "osakainstitution" }
+    department { '整形外科、リハビリテーション科' }
+    postcode { 9876543 }
+    prefecture { "大阪府" }
+    address_city { "大阪市中央区大阪下町" }
+    address_street { "9−9−9" }
+    address_building { "大阪ハイツ1F" }
+    address { "大阪府大阪市中央区大阪下町9−9−9大阪ハイツ1F" }
+  end
 end
