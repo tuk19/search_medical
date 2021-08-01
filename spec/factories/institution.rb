be29636600c08558392d70d4951e311758697cba
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-08-01 16:09:32 +0900

    fix: departmentとinstroductionを追加

--- spec/factories/institution.rb
+++ spec/factories/institution.rb
@@ -1,12 +1,14 @@
 FactoryBot.define do
   factory :tokyoinstitution, class: "Institution" do
     name { "tokyoinstitution" }
+    department { "内科" }
     postcode { 1234567 }
     prefecture { "東京都" }
     address_city { "東京区東京" }
     address_street { "1-2-3" }
     address_building { "東京ビル1F" }
     address { "東京都東京区東京1-2-3東京ビル1F" }
+    introduction { "東京駅近く" }
   end
 
   factory :fukuokainstitution, class: "Institution" do
@@ -18,5 +20,6 @@ FactoryBot.define do
     address_street { "2-9-0" }
     address_building { "福岡ハイツ1F" }
     address { "福岡県福岡市福岡区福岡2-9-0福岡ハイツ1F" }
+    introduction { "バリアフリー" }
   end
 end
