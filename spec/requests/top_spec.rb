bc9c1b141db96252840ba11d66fe0d13f98f885e
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-08-01 23:47:21 +0900

    feat: 使用法確認 ページのrequestテストを追加

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -16,4 +16,33 @@ RSpec.describe "Tops", type: :request do
     end
   end
 
+  describe "GET /user_info" do
+
+    before do
+      get top_user_info_path
+    end
+
+    example "ユーザーの使用法説明ページへアクセスできること"  do
+      expect(response).to have_http_status(:success)
+    end
+
+    example "ログイン機能が含まれること" do
+      expect(response.body).to include("お気に入りに登録すると、プロフィール画面にお気に入り一覧が表示されます。")
+    end
+  end
+
+  describe "GET /medicalstaff_info" do
+
+    before do
+      get top_medicalstaff_info_path
+    end
+
+    example "医療関係者の使用法説明ページへアクセスできること"  do
+      expect(response).to have_http_status(:success)
+    end
+
+    example "ログイン機能が含まれること" do
+      expect(response.body).to include("所属登録をすると、登録情報画面に一覧が表示されます。")
+    end
+  end
 end
