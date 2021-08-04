7783f87068732cfe1966362b8cef2bba497ebd54
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-08-04 09:08:20 +0900

    fix: font awesomeを導入してハンバーガーメニューを追加、

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import '@fortawesome/fontawesome-free/js/all';
 import "channels"
 import "jquery"
 import "packs/jquery.jpostal.js"
