4775005a5fe5fa645fc4ec35d8cbf72af7fa9202
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-08-05 21:18:14 +0900

    feat: medicalstaff_menu開閉のjsを追加

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -1,6 +1,6 @@
 jQuery(document).on("turbolinks:load", function(){
   $("#user_menu_box").on("click", (function(){
-      var $get_id = $("#user_menu_bar");
+      let $get_id = $("#user_menu_bar");
       if($get_id.hasClass("opened")){
           $get_id.css("display", "none");
           $get_id.removeClass("opened");
@@ -8,5 +8,22 @@ jQuery(document).on("turbolinks:load", function(){
           $get_id.css("display", "flex");
           $get_id.addClass("opened");
       }
+  }))
+
+  $("#medicalstaff_menu_box").on("click", (function(){
+    let $get_id = $("#medicalstaff_menu_bar");
+    let $hamburger = $("#medicalstaff_hamburger")
+    let $menu_close = $("#medicalstaff_menu_close")
+    if($get_id.hasClass("opened")){
+        $get_id.css("display", "none");
+        $get_id.removeClass("opened");
+        $hamburger.css("display", "block");
+        $menu_close.css("display", "none")
+    }else{
+        $get_id.css("display", "flex");
+        $get_id.addClass("opened");
+        $hamburger.css("display", "none");
+        $menu_close.css("display", "block")
+    }
   }))
 })
